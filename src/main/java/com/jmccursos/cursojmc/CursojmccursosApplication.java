a9b81c1b2ceda69401faa9565619b0e3b76ca3b8
jessemusic <jm_cursos@hotmail.com>
2020-04-30 17:13:27 -0300

    Projeto Criado

---
+++ src/main/java/com/jmccursos/cursojmc/CursojmccursosApplication.java
@@ -0,0 +1,13 @@
+package com.jmccursos.cursojmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursojmccursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursojmccursosApplication.class, args);
+	}
+
+}
